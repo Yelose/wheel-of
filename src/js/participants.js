0c752db53f1094a406e7dffb88ec17294c0d86eb
Marina González Suárez <yelose85@gmail.com>
2022-06-09 14:00:59 +0200

    corrected the stupid marks

---
+++ src/js/participants.js
@@ -0,0 +1,58 @@
+export let participants = [
+    {
+        name: "Tania",
+        background: "hsl(0, 100%, 50%)"
+    },
+    {
+        name: "Nerea",
+        background: "hsl(20, 100%, 50%)"
+    },
+    {
+        name: "Marina",
+        background: "hsl(40, 100%, 50%)"
+    },
+    {
+        name: "Marta",
+        background: "hsl(60, 100%, 50%)"
+    },
+    {
+        name: "Viviana",
+        background: "hsl(80, 100%, 50%)"
+    },
+    {
+        name: "Verónica",
+        background: "hsl(100, 100%, 50%)"
+    },
+    {
+        name: "Marybell",
+        background: "hsl(120, 100%, 50%)"
+    },
+    {
+        name: "Cristina",
+        background: "hsl(140, 100%, 50%)"
+    },
+    {
+        name: "Paz",
+        background: "hsl(160, 100%, 50%)"
+    },
+    {
+        name: "Juliet",
+        background: "hsl(180, 100%, 50%)"
+    },
+    {
+        name: "Esther",
+        background: "hsl(200, 100%, 50%)"    
+    },
+    {
+        name: "Iria",
+        background: "hsl(220, 100%, 50%)"
+    },
+    {
+        name: "Laura",
+        background: "hsl(220, 100%, 50%)"
+    },
+    {
+        name: "Sara",
+        background: "hsl(240, 100%, 50%)"
+    }
+]
