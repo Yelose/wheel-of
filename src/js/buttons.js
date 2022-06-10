f58c500b5b7d90fb783fa36c8f253b8cae65d11d
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-06-10 12:23:25 +0200

    delete buttons

--- src/js/buttons.js
+++
@@ -1,9 +0,0 @@
-let btnStart = document.getElementById('start btn')
-let btnReset = document.getElementById('reset btn')
-let btnUsers = document.getElementById('users btn')
-let carousel = document.getElementById('carousel')
-btnStart.addEventListener('click', startWheel());
-
-function startWheel(){
-    document.querySelector('carousel')
-}
