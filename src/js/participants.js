6e62cf11fc8d96f9fadda2de1f6a6517b7eb3ab8
Marina González Suárez <yelose85@gmail.com>
2022-06-14 15:34:09 +0200

    added Id to the new users, printing them from the start

--- src/js/participants.js
+++ src/js/participants.js
@@ -1,45 +1,34 @@
 
-export let participants = [
-    {
-        name: "Tania",
-    },
-    {
-        name: "Nerea",
-    },
-    {
-        name: "Marina",
-    },
-    {
-        name: "Marta",
-    },
-    {
-        name: "Viviana",
-    },
-    {
-        name: "Vero",
-    },
-    {
-        name: "Mary Bell"
-    },
-    {
-        name: "Cris"
-    },
-    {
-        name: "Paz"
-    },
-    {
-        name: "Juliet"
-    },
-    {
-        name: "Esther"
-    },
-    {
-        name: "Iria"
-    },
-    {
-        name: "Laura"
-    },
-    {
-        name: "Sara"
-    }
-]
+export let participants = []
+
+export function participantsDefault(){
+    let total = participants.length ;
+    participants.push({ name: "Tania", id: total})
+    total = participants.length 
+    participants.push({ name: "Nerea", id: total})
+    total = participants.length
+    participants.push({ name: "Marina", id: total})
+    total = participants.length
+    participants.push({ name: "Marta", id: total})
+    total = participants.length
+    participants.push({ name: "Viviana", id: total})
+    total = participants.length
+    participants.push({ name: "Vero", id: total})
+    total = participants.length
+    participants.push({ name: "MaryBel", id: total})
+    total = participants.length
+    participants.push({ name: "Cris", id: total})
+    total = participants.length
+    participants.push({ name: "Paz", id: total})
+    total = participants.length
+    participants.push({ name: "Juliet", id: total})
+    total = participants.length
+    participants.push({ name: "Esther", id: total})
+    total = participants.length
+    participants.push({ name: "Iria", id: total})
+    total = participants.length
+    participants.push({ name: "Laura", id: total})
+    total = participants.length
+    participants.push({ name: "Sara", id: total})
+}
+participantsDefault ()
