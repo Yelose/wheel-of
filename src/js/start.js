132f5b52920e56693bfad96929b44483178e6179
Marina González Suárez <yelose85@gmail.com>
2022-06-17 08:52:20 +0200

    cant switch

--- src/js/start.js
+++ src/js/start.js
@@ -6,9 +6,6 @@ import { participants } from "./participants"
 
 export function start(){
     var randomNumber = Math.floor(Math.random()*participants.length)
-    // var sacrifice = participants[Math.floor(Math.random()*participants.length)]
-    // const element = sacrifice.name;
-    // const sacrifId = sacrifice.id;
     deleteUser(participants[randomNumber].id)
 }
 
